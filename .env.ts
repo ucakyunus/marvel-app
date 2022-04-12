8a8b8f1fd5cef9198b1ff5690cfa36b1f0d51394
yunusu <yunusu@kariyer.net>
2022-04-12 06:58:47 +0300

    feat: remove gitignore

---
+++ .env.ts
@@ -0,0 +1,3 @@
+export const BASE_API_URL = 'https://gateway.marvel.com';
+export const PUBLIC_API_KEY = '021f1694ace5620bfd97fa53c56d07c5';
+export const PRIVATE_API_KEY = '8ffd761aa8599c01cfb9463d91e65791a40f2e66';
